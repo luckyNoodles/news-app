e0cd6df926e91f7ce1020787a43b021b47fe64ee
scottCheung <scottcx2@gmail.com>
2023-02-03 01:21:08 -0500

    revert from test with map over story

--- src/Components/Story.js
+++ src/Components/Story.js
@@ -2,32 +2,35 @@ function Story (storyData) {
 
     console.log(storyData);
     const storiesArray = storyData.data.results;
-    const story1 = storiesArray[5];
-    const storyImageArray = story1.multimedia[1];
+    const story = storiesArray[5];
+    const storyImageArray = story.multimedia[1];
 
     return (
         <div className="storyComponent">
-                       
+
+    
             <div className="storyBox">
                
                 <div className="imageContainer">
-                    <a href={ story1.url } >
+                    <a href={ story.url } >
                         <img src={ storyImageArray.url } alt={ storyImageArray.type } />
                     </a>
                 </div>
 
                 <div className="storyText">
                     <div className="title">
-                        <a href={ story1.url }>
-                            <h3>{ storiesArray[5].title }</h3>
+                        <a href={ story.url }>
+                            <h3>{ story.title }</h3>
                         </a>
                     </div>
                     <div className="description">
-                        <p> { storiesArray[5].abstract }</p>
+                        <p> { story.abstract }</p>
                     </div>
                 </div>
 
             </div>
+
+              
         </div>
         
     )
