f32bb0a3e39d93e482a2050ac5f4ff306a0501d5
scottCheung <scottcx2@gmail.com>
2023-02-03 17:53:13 -0500

    updated story with images, date, by line, section

--- src/Components/Story.js
+++ src/Components/Story.js
@@ -2,18 +2,8 @@ import { useEffect, useState } from 'react';
 
 function Story () {
 
-    // console.log(storyData);
-    // const storiesArray = storyData.data.results;
-    // const story = storiesArray[5];
-    // const storyImageArray = story.multimedia[1];
-
 const [stories, setStories] = useState([]);
 console.log(stories);
-// const story1 = stories[1];
-// const image1 = story1.multimedia[1];
-// console.log(image1);
-// console.log(stories);
-
 
 useEffect(() => {
 
@@ -46,33 +36,40 @@ useEffect(() => {
                // const images = stories
                stories.map((story) => {
 
-               const imageArray = story.multimedia; 
-               const image = imageArray == null ? "no image available" : imageArray[1]; 
-            //    const storyImage = imageArray;
-                console.log(image);
-              return  (<div className="storyBox" key={story.uri}>
-                
-                    <div className="imageContainer">
-                        <a href={ story.url } >
-                            <img src={ image.url } alt={ image.type } />
-                        </a>
-                    </div>
+                    const imageArray = story.multimedia; 
+                    const image = imageArray == null ? "missing media" : imageArray[1]; 
+                    
+                    const pubDate = new Date(story.published_date);
+                    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
+                    const date = pubDate.toLocaleDateString('US-EN', options);
 
-                    <div className="storyText">
-                        <div className="title">
-                            <a href={ story.url }>
-                                <h3>{ story.title }</h3>
-                            </a>
+                    return  (<div className="storyBox" key={story.uri}>
+                        
+                            <div className="imageContainer">
+                                <a href={ story.url } >
+                                    <img src={ image.url ? image.url : '.././images/newYorkTimes.jpg' } alt={ image.caption } />
+                                </a>
+                                <p>{ image.copyright ? `Photo by:${image.copyright}` : "" }</p>
+                            </div>
+
+                            <div className="storyText">
+                                <div className="title">
+                                    
+                                    <h4>{ story.section }</h4>
+                                    <a href={ story.url }>
+                                        <h3>{ story.title }</h3>
+                                        <p>{ story.byline }</p>
+                                    </a>
+                                </div>
+                                
+                                <div className="description">
+                                    <p>{ date }</p>
+                                    <p> { story.abstract }</p>
+                                </div>
+                            </div>
+
                         </div>
-                        <div className="description">
-                            <p> { story.abstract }</p>
-                        </div>
-                    </div>
-
-                </div>
-              )
-
-
+                    )
 
                 })
            }
