890348ac2f985094a8dfce6e1061dc425d02b05e
scottCheung <scottcx2@gmail.com>
2023-02-04 03:23:36 -0500

    successfully passed url to story component and triggered a different fetch for each section, using only one component for every section

--- src/App.js
+++ src/App.js
@@ -4,8 +4,13 @@ import './App.scss';
 import NavBar from './Components/NavBar.js';
 import Story from './Components/Story';
 
-//Mock components
-const Home = () => <h2>Home</h2>
+
+// NYT section URLS
+const apiKey = 'n92DZWsbGi92AGqOU61WrbTbzwcLBSKT';
+const topStoriesUrl = `https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`;
+const businessUrl = 'https://api.nytimes.com/svc/topstories/v2/business.json?api-key=n92DZWsbGi92AGqOU61WrbTbzwcLBSKT';
+const techUrl = 'https://api.nytimes.com/svc/topstories/v2/technology.json?api-key=n92DZWsbGi92AGqOU61WrbTbzwcLBSKT';
+const booksUrl = 'https://api.nytimes.com/svc/topstories/v2/books.json?api-key=n92DZWsbGi92AGqOU61WrbTbzwcLBSKT';
 
 function App() {                    
 
@@ -14,19 +19,22 @@ function App() {
       <header className="App-header">
         <div className="App-wrapper">
 
-          <Routes> 
-            <Route path="/" element={ <Home /> } />
-          </Routes>
-
              <NavBar /> 
-
         </div>
       </header>
-            <h2>Breaking News</h2>
-            <Story />
+      
+      <main>
+        <h2>Breaking News</h2>
+            
+            <Routes> 
+              <Route path="/" element={ <Story api={ topStoriesUrl} /> } />
+              <Route path="/business" element={ <Story api={ businessUrl} /> } />
+              <Route path="/technology" element={ <Story api={ techUrl } /> } />
+              <Route path="/books" element={ <Story apiKey={ booksUrl } /> } />
+            </Routes>
 
-        
-     
+      </main>
+
     </div>
   );
 }
--- src/Components/Story.js
+++ src/Components/Story.js
@@ -1,41 +1,35 @@
 import { useEffect, useState } from 'react';
 
-function Story () {
+function Story ({api}) {
+    const [stories, setStories] = useState([]);
+    console.log(stories);
 
-const [stories, setStories] = useState([]);
-console.log(stories);
-
-useEffect(() => {
-
-    const fetchData = async () => {
-      const apiKey = 'n92DZWsbGi92AGqOU61WrbTbzwcLBSKT';
-      const url = new URL(`https://api.nytimes.com/svc/topstories/v2/home.json?api-key=${apiKey}`)
-
-      try {
-        const data = await fetch(url);
-        const response = await data.json();
-        const resultsArray = response;
-        // console.log(response);
-        setStories(resultsArray.results);
-        // console.log(stories);
-
-      } catch (error) {
-        console.log(error);
-      }
-    } 
-    fetchData();
+    // const apiKey = 'n92DZWsbGi92AGqOU61WrbTbzwcLBSKT';
     
-  },[]);
+    useEffect(() => {
+        const fetchData = async () => {
+            const url = api;
+            console.log(api);
+        
+        try {
+            const data = await fetch(api);
+            const response = await data.json();
+            const resultsArray = response;
+            // console.log(response);
+            setStories(resultsArray.results);
+            // console.log(stories);
+            } catch (error) {
+                console.log(error);
+            }
+        } 
+        fetchData();
+        
+    },[api]);
  
-
     return (
         <div className="storyComponent">
-           
-           {
-               
-               // const images = stories
+            {
                stories.map((story) => {
-
                     const imageArray = story.multimedia; 
                     const image = imageArray == null ? "missing media" : imageArray[1]; 
                     
@@ -43,42 +37,34 @@ useEffect(() => {
                     const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric'};
                     const date = pubDate.toLocaleDateString('US-EN', options);
 
-                    return  (<div className="storyBox" key={story.uri}>
-                        
-                            <div className="imageContainer">
-                                <a href={ story.url } >
-                                    <img src={ image.url ? image.url : '.././images/newYorkTimes.jpg' } alt={ image.caption } />
-                                </a>
-                                <p>{ image.copyright ? `Photo by:${image.copyright}` : "" }</p>
-                            </div>
-
-                            <div className="storyText">
-                                <div className="title">
-                                    
-                                    <h4>{ story.section }</h4>
-                                    <a href={ story.url }>
-                                        <h3>{ story.title }</h3>
-                                        <p>{ story.byline }</p>
-                                    </a>
-                                </div>
+                return  (<div className="storyBox" key={story.uri}>
+        
+                        <div className="imageContainer">
+                            <a href={ story.url } >
+                                <img src={ image.url ? image.url : '.././images/newYorkTimes.jpg' } alt={ image.caption } />
+                            </a>
+                            <p>{ image.copyright ? `Photo by:${image.copyright}` : "" }</p>
+                        </div>
+                        <div className="storyText">
+                            <div className="title">
                                 
+                                <h4>{ story.section }</h4>
+                                <a href={ story.url }>
+                                    <h3>{ story.title }</h3>
+                                    <p>{ story.byline }</p>
+                                </a>
+                            </div>
                                 <div className="description">
                                     <p>{ date }</p>
                                     <p> { story.abstract }</p>
                                 </div>
                             </div>
-
                         </div>
                     )
-
                 })
            }
-
-              
         </div>
-        
     )
-
 }
 
 export default Story;
