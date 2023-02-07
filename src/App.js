119b0a675bffb0e3f8652383d9191260bcd3f90b
scottCheung <scottcx2@gmail.com>
2023-02-07 09:51:30 -0500

    updated spaces

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
-import { useParams } from "react-router-dom";
+// import { useParams } from "react-router-dom";
 import './App.scss';
 import NavBar from './Components/NavBar.js';
 import SectionRouter from './Components/SectionRouter'; 
 import BackToTop from './Components/BackToTop';
 
 function App() {   
-  let { section } = useParams(); 
-  console.log(section);
+  // let { section } = useParams(); 
+  // console.log(section);
   return (
     <div className="App">
+      
       <header className="App-header">
         <div className="App-wrapper">
           <NavBar /> 
         </div>
-        <h2>{section}</h2>
       </header>
+
       <main className="App-wrapper"> 
         <div className="back">
           <BackToTop /> 
         </div>
         <SectionRouter />
       </main>
+      
     </div>
   );
 }
