7ebc3504e6b707402f354716cb605927d0694934
scottCheung <scottcx2@gmail.com>
2023-12-15 23:03:50 -0500

    initialized api call in app.js

--- netlify/functions/getApiKey.js
+++ netlify/functions/getApiKey.js
@@ -1,18 +1,12 @@
 exports.handler = async (event, context) => {
-    try {
-        const apiKey = process.env.REACT_APP_API_KEY
-        
-        return {
-            statusCode:200,
-            body: JSON.stringify( {apiKey} )
+  try {
+    const apiKey = process.env.REACT_APP_API_KEY;
 
-        };
-    }
-    catch (error) {
-        return {
-            statusCode: 500,
-            body: JSON.stringify( {error: 'Failed to retrieve API key'})
-        };
-    }
-
-}
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ apiKey }),
+    };
+  } catch (error) {
+    console.error(error);
+  }
+};
--- src/Components/Fetch.js
+++ src/Components/Fetch.js
@@ -1,32 +1,27 @@
-import { useEffect, useState } from 'react';
-import Story from './Story';
+import { useEffect, useState } from "react";
+import Story from "./Story";
 
-function Fetch ( { api } ) {
-    const [stories, setStories] = useState([]);
-   
-    useEffect(() => {
-        const fetchData = async () => {
+function Fetch({ api }) {
+  const [stories, setStories] = useState([]);
 
-        try {
-            const data = await fetch(api);
-            const response = await data.json();
-            const resultsArray = response;
-            
-            setStories(resultsArray.results);
-            } 
-            catch (error) {
-                alert(`An error occured while fetching data ${error}. Please try again.`);
-            }
-        } 
-        fetchData();
-        
-    },[api]);
- 
-    return (
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await fetch(api);
+        const response = await data.json();
+        const resultsArray = response;
 
-        <Story apiData={stories}/>
-       
-    )
+        setStories(resultsArray.results);
+      } catch (error) {
+        alert(
+          `An error occured while fetching data ${error}. Please try again.`
+        );
+      }
+    };
+    fetchData();
+  }, [api]);
+
+  return <Story apiData={stories} />;
 }
 
 export default Fetch;
