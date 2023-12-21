480fab307cfe4419c0a43ce3d997cd5adbdf1994
scottCheung <scottcx2@gmail.com>
2023-12-21 02:07:43 -0500

    dev test

--- netlify/functions/nodeProxy.js
+++ netlify/functions/nodeProxy.js
@@ -1,11 +1,11 @@
 exports.handler = async (event, context) => {
   try {
     const requestBody = JSON.parse(event.body);
-    const { section, apikey } = requestBody;
-    // const apiKey = process.env.REACT_APP_API_KEY;
+    const { section } = requestBody;
+    const apiKey = process.env.REACT_APP_API_KEY;
 
     const nytApi = await fetch(
-      `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${apikey}`
+      `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${apiKey}`
     );
 
 
