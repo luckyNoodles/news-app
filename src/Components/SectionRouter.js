7b59c7076bbd1ab76e2841c1a1d2f34315eae68d
scottCheung <scottcx2@gmail.com>
2023-12-21 20:55:00 -0500

    updated cors restrictions

--- src/Components/SectionRouter.js
+++ src/Components/SectionRouter.js
@@ -3,10 +3,8 @@ import Fetch from "./Fetch";
 import ErrorPage from "./ErrorPage";
 
 function SectionRouter() {
-  // const apikey = props.apikey;
+ 
   const proxyUrl ="https://daily-times-headlines.netlify.app/.netlify/functions/nodeProxy";
-  // const proxyUrl = "https://dev-news-app.netlify.app/.netlify/functions/nodeProxy";
-  // const proxyUrl = "http://localhost:8888/.netlify/functions/nodeProxy";
 
   return (
     <div>
