dcba161c2c8dd359146ef7584ce4259ab83f6ab2
scottCheung <scottcx2@gmail.com>
2023-12-21 20:09:26 -0500

    api check

--- src/Components/SectionRouter.js
+++ src/Components/SectionRouter.js
@@ -6,8 +6,8 @@ function SectionRouter() {
 
   // const apikey = props.apikey;
 
-  // const proxyUrl = "https://daily-times-headlines.netlify.app/.netlify/functions/nodeProxy";
-   const proxyUrl = "https://dev-news-app.netlify.app/.netlify/functions/nodeProxy";
+  const proxyUrl = "https://daily-times-headlines.netlify.app/.netlify/functions/nodeProxy";
+  //  const proxyUrl = "https://dev-news-app.netlify.app/.netlify/functions/nodeProxy";
     // const proxyUrl = "http://localhost:8888/.netlify/functions/nodeProxy";
 
 
