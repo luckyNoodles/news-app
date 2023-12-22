936ef31cfd8c4cf4e47cb1015b07463f5aefd223
scottCheung <scottcx2@gmail.com>
2023-12-21 19:32:34 -0500

    api check

--- src/Components/SectionRouter.js
+++ src/Components/SectionRouter.js
@@ -2,12 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Fetch from "./Fetch";
 import ErrorPage from "./ErrorPage";
 
-function SectionRouter(props) {
+function SectionRouter() {
 
-  const apikey = props.apikey;
+  // const apikey = props.apikey;
 
-  // const proxyUrl = "https://daily-times-headlines.netlify.app/.netlify/functions/nodeProxy";
-   const proxyUrl = "https://dev-news-app.netlify.app/.netlify/functions/nodeProxy";
+  const proxyUrl = "https://daily-times-headlines.netlify.app/.netlify/functions/nodeProxy";
+  //  const proxyUrl = "https://dev-news-app.netlify.app/.netlify/functions/nodeProxy";
     // const proxyUrl = "http://localhost:8888/.netlify/functions/nodeProxy";
 
 
@@ -16,90 +16,90 @@ function SectionRouter(props) {
       <Routes>
         <Route
           path="/"
-          element={<Fetch section={"home"} apiUrl={proxyUrl} apikey={apikey} />}
+          element={<Fetch section={"home"} apiUrl={proxyUrl}  />}
         />
         <Route
           path="/world"
           element={
-            <Fetch section={"world"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"world"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/us"
-          element={<Fetch sectionl={"us"} apiUrl={proxyUrl} apikey={apikey} />}
+          element={<Fetch sectionl={"us"} apiUrl={proxyUrl}  />}
         />
         <Route
           path="/politics"
           element={
-            <Fetch section={"politics"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"politics"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/nyregion"
           element={
-            <Fetch section={"nyregion"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"nyregion"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/business"
           element={
-            <Fetch section={"business"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"business"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/opinion"
           element={
-            <Fetch section={"opinion"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"opinion"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/science"
           element={
-            <Fetch section={"science"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"science"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/health"
           element={
-            <Fetch section={"health"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"health"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/sports"
           element={
-            <Fetch section={"sports"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"sports"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/arts"
-          element={<Fetch section={"arts"} apiUrl={proxyUrl} apikey={apikey} />}
+          element={<Fetch section={"arts"} apiUrl={proxyUrl}  />}
         />
         <Route
           path="/books"
           element={
-            <Fetch section={"books"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"books"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/style"
           element={
-            <Fetch section={"style"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"style"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/food"
-          element={<Fetch section={"food"} apiUrl={proxyUrl} apikey={apikey} />}
+          element={<Fetch section={"food"} apiUrl={proxyUrl}  />}
         />
         <Route
           path="/travel"
           element={
-            <Fetch section={"travel"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"travel"} apiUrl={proxyUrl}  />
           }
         />
         <Route
           path="/magazine"
           element={
-            <Fetch section={"magazine"} apiUrl={proxyUrl} apikey={apikey} />
+            <Fetch section={"magazine"} apiUrl={proxyUrl}  />
           }
         />
         <Route path="*" element={<ErrorPage />} />
